e93e1edfedf2912b46fbcd766e2df3893ca1a804
Ivan Grekov <ivan.grekov@windmill.digital>
2023-11-20 21:42:01 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { NativeBaseProvider, Box } from 'native-base';
+
+import { THEME, STYLE_VARIABLES } from './src/constants/style';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <NativeBaseProvider theme={THEME}>
+                <View style={styles.container}>
+                    <StatusBar style="auto" backgroundColor={STYLE_VARIABLES.bgColor} />
+
+                    <Box style={styles.appWrapper}>
+                        <Text>Hello</Text>
+                        {/* <Routes homeScreenChildren={<Header />} /> */}
+                    </Box>
+                </View>
+            </NativeBaseProvider>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    appWrapper: {
+        width: 400,
+        height: 700,
+        backgroundColor: STYLE_VARIABLES.bgColor,
+    },
 });
